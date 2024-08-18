0cd8231c86afe834befbafb72b23b5247066dc64
wineway <wangyuweihx@gmail.com>
2024-08-18 16:00:33 +0800

    impl hts ringbuffer
    Signed-off-by: wineway <wangyuweihx@gmail.com>

--- src/common/ring.hpp
+++ src/common/ring.hpp
@@ -7,6 +7,9 @@
 
 #include "log.hpp"
 
+static_assert(sizeof(std::atomic<uint32_t>) == sizeof(uint32_t), "");
+static_assert(sizeof(std::atomic<uint64_t>) == sizeof(uint64_t), "");
+
 /**
  * Compiler barrier.
  *
@@ -32,6 +35,12 @@ static inline void sqk_pause(void) {
 #endif
 
 #define SQK_CACHELINE_ALIGNED alignas(SQK_CACHE_LINESIZE)
+
+/**
+ *  * definition to mark a variable or function parameter as used so
+ *   * as to avoid a compiler warning
+ *    */
+#define SQK_SET_USED(x) (void)(x)
 
 /**
  * Force alignment
@@ -106,16 +115,74 @@ static inline uint32_t sqk_align32pow2(uint32_t x) {
 
 struct MemZone {
     uint8_t* addr_;
+    ssize_t size_;
 };
 
-struct RingHeadTail {
-    std::atomic<uint32_t> head_;
-    std::atomic<uint32_t> tail_;
+struct RingHeadTail_Normal {
+    volatile uint32_t head_;
+    volatile uint32_t tail_;
 };
 
-template<typename T, typename Allocator = std::allocator<uint8_t>>
-    requires(sizeof(T) % 4 == 0)
-struct MpscRing {
+union RingRtsPosCnt {
+    alignas(sizeof(uint64_t)) std::atomic<uint64_t> raw_;
+
+    struct {
+        uint32_t head_;
+        uint32_t tail_;
+    } val;
+};
+
+union RingHtsPos {
+    alignas(sizeof(uint64_t)) std::atomic<uint64_t> raw_;
+
+    struct {
+        std::atomic<uint32_t> head_;
+        std::atomic<uint32_t> tail_;
+    } pos;
+
+    RingHtsPos() {}
+};
+
+union RingHtsPosPlain {
+    alignas(sizeof(uint64_t)) uint64_t raw_;
+
+    struct {
+        uint32_t head_;
+        uint32_t tail_;
+    } pos;
+};
+
+union RingHeadTail {
+    struct {
+        volatile uint32_t head_;
+        volatile uint32_t tail_;
+    };
+
+    RingHtsPos ht;
+
+    struct {
+        volatile RingRtsPosCnt head_;
+        uint32_t htd_max_;
+        volatile RingRtsPosCnt tail_;
+    } rts;
+};
+
+enum class RingSyncType {
+    SQK_RING_SYNC_MT, /**< multi-thread safe (default mode) */
+    SQK_RING_SYNC_ST, /**< single thread only */
+    SQK_RING_SYNC_MT_RTS, /**< multi-thread relaxed tail sync */
+    SQK_RING_SYNC_MT_HTS, /**< multi-thread head/tail sync */
+};
+
+template<
+    typename T,
+    RingSyncType prod_sync_type = RingSyncType::SQK_RING_SYNC_MT,
+    RingSyncType cons_sync_type = RingSyncType::SQK_RING_SYNC_ST,
+    bool transactional_prod = 0,
+    bool transactional_cons = 0,
+    typename Allocator = std::allocator<uint8_t>>
+    requires(sizeof(T) % 4 == 0) && std::is_trivially_copyable_v<T>
+struct Ring {
   private:
     uint32_t size; /**< Size of ring. */
     uint32_t mask; /**< Mask (size-1) of ring. */
@@ -127,20 +194,25 @@ struct MpscRing {
     SQK_CACHELINE_ALIGNED uint8_t pad1;
     RingHeadTail cons_;
     SQK_CACHELINE_ALIGNED uint8_t pad2;
-    MpscRing() = delete;
-    ~MpscRing() = delete;
+    Ring() = delete;
+    ~Ring() = delete;
     using allocator_traits = std::allocator_traits<Allocator>;
     /* by default set head/tail distance as 1/8 of ring capacity */
     static constexpr uint32_t HTD_MAX_DEF = 8;
 
     void init(uint32_t sz) {
-        std::memset(this, 0, sizeof(MpscRing<T>));
+        std::memset(this, 0, sizeof(Ring<T>));
         this->size = sz;
         this->mask = sz - 1;
         this->capacity = this->mask;
         this->htd_max_ = this->capacity / HTD_MAX_DEF;
     }
 
+    template<RingSyncType sync_type = prod_sync_type>
+        requires(
+            sync_type == RingSyncType::SQK_RING_SYNC_MT
+            || sync_type == RingSyncType::SQK_RING_SYNC_ST
+        )
     uint32_t move_prod_head(
         uint32_t n,
         uint32_t& old_head,
@@ -153,33 +225,135 @@ struct MpscRing {
             n = max;
             old_head = this->prod_.head_;
             /* add rmb barrier to avoid load/load reorder in weak
-		     * memory model. It is noop on x86
-		     */
+             * memory model. It is noop on x86
+             */
             sqk_smp_rmb();
             /*
-		     *  The subtraction is done between two unsigned 32bits value
-		     * (the result is always modulo 32 bits even if we have
-		     * *old_head > cons_tail). So 'free_entries' is always between 0
-		     * and capacity (which is < size).
-		     */
+             *  The subtraction is done between two unsigned 32bits value
+             * (the result is always modulo 32 bits even if we have
+             * *old_head > cons_tail). So 'free_entries' is always between 0
+             * and capacity (which is < size).
+             */
             free_entries = (this->capacity + this->cons_.tail_ - old_head);
             /* check that we have enough room in ring */
             if (unlikely(n > free_entries)) {
-                n = free_entries;
+                if constexpr (transactional_prod) {
+                    n = 0;
+                } else {
+                    n = free_entries;
+                }
             }
             if (n == 0) {
                 return 0;
             }
             new_head = old_head + n;
-            // TODO: review this
-            success = std::atomic_compare_exchange_strong_explicit(
-                &this->prod_.head_,
-                &old_head,
-                new_head,
-                std::memory_order_release,
-                std::memory_order_relaxed
-            );
+            if constexpr (prod_sync_type == RingSyncType::SQK_RING_SYNC_MT) {
+                success = std::atomic_compare_exchange_weak(
+                    reinterpret_cast<volatile std::atomic<uint32_t>*>(
+                        &this->prod_.head_
+                    ),
+                    &old_head,
+                    new_head
+                );
+            } else {
+                this->prod_.head_ = new_head, success = 1;
+            }
         } while (unlikely(success == 0));
+        return n;
+    }
+
+    template<RingSyncType sync_type = prod_sync_type>
+        requires(sync_type == RingSyncType::SQK_RING_SYNC_MT_RTS)
+    uint32_t move_prod_head(
+        uint32_t n,
+        uint32_t& old_head,
+        uint32_t& new_head,
+        uint32_t& free_entries
+    ) {}
+
+    void hts_head_wait(const RingHtsPos& ht, RingHtsPosPlain& p) {
+        while (p.pos.head_ != p.pos.tail_) {
+            sqk_pause();
+            p.raw_ =
+                std::atomic_load_explicit(&ht.raw_, std::memory_order_acquire);
+        }
+    }
+
+    template<
+        RingSyncType p_sync_type = prod_sync_type,
+        RingSyncType c_sync_type = cons_sync_type>
+        requires(
+            p_sync_type == RingSyncType::SQK_RING_SYNC_MT_HTS
+            || c_sync_type == RingSyncType::SQK_RING_SYNC_MT_HTS
+        )
+    void update_tail(
+        RingHeadTail& ht,
+        uint32_t old_tail,
+        uint32_t num,
+        uint32_t enqueue
+    ) {
+        uint32_t tail;
+
+        SQK_SET_USED(enqueue);
+
+        tail = old_tail + num;
+        std::atomic_store_explicit(
+            &ht.ht.pos.tail_,
+            tail,
+            std::memory_order_release
+        );
+    }
+
+    template<RingSyncType sync_type = prod_sync_type>
+        requires(sync_type == RingSyncType::SQK_RING_SYNC_MT_HTS)
+    uint32_t
+    move_prod_head(uint32_t num, uint32_t& old_head, uint32_t& free_entries) {
+        uint32_t n;
+        RingHtsPosPlain np, op;
+        const uint32_t capacity = this->capacity;
+        op.raw_ = std::atomic_load_explicit(
+            &this->prod_.ht.raw_,
+            std::memory_order_acquire
+        );
+        do {
+            n = num;
+            /*
+             *       * wait for tail to be equal to head,
+             *               * make sure that we read prod head/tail *before*
+             *                       * reading cons tail.
+             *                               */
+            this->hts_head_wait(this->prod_.ht, op);
+            /*
+             *  The subtraction is done between two unsigned 32bits value
+             * (the result is always modulo 32 bits even if we have
+             * *old_head > cons_tail). So 'free_entries' is always between 0
+             * and capacity (which is < size).
+             */
+            free_entries = capacity + this->cons_.tail_ - op.pos.head_;
+
+            /* check that we have enough room in ring */
+            if (unlikely(n > free_entries)) {
+                if constexpr (transactional_prod) {
+                    n = 0;
+                } else {
+                    n = free_entries;
+                }
+            }
+
+            if (n == 0)
+                break;
+
+            np.pos.tail_ = op.pos.tail_;
+            np.pos.head_ = op.pos.head_ + n;
+        } while (std::atomic_compare_exchange_strong_explicit(
+                     &this->prod_.ht.raw_,
+                     &op.raw_,
+                     np.raw_,
+                     std::memory_order_acquire,
+                     std::memory_order_acquire
+                 )
+                 == 0);
+        old_head = op.pos.head_;
         return n;
     }
 
@@ -306,7 +480,17 @@ struct MpscRing {
         this->enqueue_elems_32(nr_size, nr_idx, entries, nr_num);
     }
 
+    template<
+        RingSyncType p_sync_type = prod_sync_type,
+        RingSyncType c_sync_type = cons_sync_type>
+        requires(
+            p_sync_type == RingSyncType::SQK_RING_SYNC_MT
+            || p_sync_type == RingSyncType::SQK_RING_SYNC_ST
+            || c_sync_type == RingSyncType::SQK_RING_SYNC_MT
+            || c_sync_type == RingSyncType::SQK_RING_SYNC_ST
+        )
     void update_tail(
+        RingHeadTail& ht,
         uint32_t old_val,
         uint32_t new_val,
         uint32_t single,
@@ -317,18 +501,83 @@ struct MpscRing {
         else
             sqk_smp_rmb();
         /*
-	 * If there are other enqueues/dequeues in progress that preceded us,
-	 * we need to wait for them to complete
-	 */
+         * If there are other enqueues/dequeues in progress that preceded us,
+         * we need to wait for them to complete
+         */
         if (!single)
             while (unlikely(this->prod_.tail_ != old_val))
                 sqk_pause();
 
-        this->prod_.tail_ = new_val;
+        ht.tail_ = new_val;
     }
 
+    template<RingSyncType sync_type = cons_sync_type>
+        requires(sync_type == RingSyncType::SQK_RING_SYNC_MT_HTS)
+    uint32_t
+    move_cons_head(uint32_t num, uint32_t& old_head, uint32_t& entries) {
+        uint32_t n;
+        RingHtsPosPlain np, op;
+
+        op.raw_ = std::atomic_load_explicit(
+            &this->cons_.ht.raw_,
+            std::memory_order_acquire
+        );
+        do {
+            /* Restore n as it may change every loop */
+            n = num;
+
+            /*
+             * wait for tail to be equal to head,
+             * make sure that we read cons head/tail *before*
+             * reading prod tail.
+             */
+            this->hts_head_wait(this->cons_.ht, op);
+
+            /* The subtraction is done between two unsigned 32bits value
+             * (the result is always modulo 32 bits even if we have
+             * cons_head > prod_tail). So 'entries' is always between 0
+             * and size(ring)-1.
+             */
+            entries = this->prod_.tail_ - op.pos.head_;
+
+            /* Set the actual entries for dequeue */
+            if (n > entries) {
+                if constexpr (transactional_cons) {
+                    n = 0;
+                } else {
+                    n = entries;
+                }
+            }
+
+            if (unlikely(n == 0))
+                break;
+
+            np.pos.tail_ = op.pos.tail_;
+            np.pos.head_ = op.pos.head_ + n;
+            /*
+             * this CAS(ACQUIRE, ACQUIRE) serves as a hoist barrier to prevent:
+             *  - OOO reads of prod tail value
+             *  - OOO copy of elems from the ring
+             */
+        } while (std::atomic_compare_exchange_strong_explicit(
+                     &this->cons_.ht.raw_,
+                     &op.raw_,
+                     np.raw_,
+                     std::memory_order_acquire,
+                     std::memory_order_acquire
+                 )
+                 == 0);
+        old_head = op.pos.head_;
+        return n;
+    }
+
+    template<RingSyncType sync_type = cons_sync_type>
+        requires(
+            sync_type == RingSyncType::SQK_RING_SYNC_MT
+            || sync_type == RingSyncType::SQK_RING_SYNC_ST
+        )
     uint32_t move_cons_head(
-        unsigned int n,
+        uint32_t n,
         uint32_t& old_head,
         uint32_t& new_head,
         uint32_t& entries
@@ -344,28 +593,43 @@ struct MpscRing {
             old_head = this->cons_.head_;
 
             /* add rmb barrier to avoid load/load reorder in weak
-		 * memory model. It is noop on x86
-		 */
+             * memory model. It is noop on x86
+             */
             sqk_smp_rmb();
 
             /* The subtraction is done between two unsigned 32bits value
-		 * (the result is always modulo 32 bits even if we have
-		 * cons_head > prod_tail). So 'entries' is always between 0
-		 * and size(ring)-1.
-		 */
+             * (the result is always modulo 32 bits even if we have
+             * cons_head > prod_tail). So 'entries' is always between 0
+             * and size(ring)-1.
+             */
             entries = (this->prod_.tail_ - old_head);
 
             /* Set the actual entries for dequeue */
-            if (n > entries)
-                n = entries;
+            if (n > entries) {
+                if constexpr (transactional_cons) {
+                    n = 0;
+                } else {
+                    n = entries;
+                }
+            }
 
             if (unlikely(n == 0))
                 return 0;
 
             new_head = old_head + n;
-            this->cons_.head_ = new_head;
-            sqk_smp_rmb();
-            success = 1;
+            if constexpr (cons_sync_type == RingSyncType::SQK_RING_SYNC_ST) {
+                this->cons_.head_ = new_head;
+                sqk_smp_rmb();
+                success = 1;
+            } else {
+                success = std::atomic_compare_exchange_weak(
+                    reinterpret_cast<volatile std::atomic<uint32_t>*>(
+                        &this->cons_.head_
+                    ),
+                    &old_head,
+                    new_head
+                );
+            }
         } while (unlikely(success == 0));
         return n;
     }
@@ -469,30 +733,18 @@ struct MpscRing {
 
     template<typename T2 = T>
         requires(sizeof(T) == 8)
-    void dequeue_elements(
-        uint32_t cons_head,
-        void* entries,
-        uint32_t num
-    ) {
+    void dequeue_elements(uint32_t cons_head, void* entries, uint32_t num) {
         this->dequeue_elems_64(cons_head, entries, num);
     }
 
     template<typename T2 = T>
         requires(sizeof(T) == 16)
-    void dequeue_elements(
-        uint32_t cons_head,
-        void* entries,
-        uint32_t num
-    ) {
+    void dequeue_elements(uint32_t cons_head, void* entries, uint32_t num) {
         this->dequeue_elems_128(cons_head, entries, num);
     }
 
     template<typename T2 = T>
-    void dequeue_elements(
-        uint32_t cons_head,
-        void* entries,
-        uint32_t num
-    ) {
+    void dequeue_elements(uint32_t cons_head, void* entries, uint32_t num) {
         uint32_t idx, scale, nr_idx, nr_num, nr_size;
 
         /* Normalize to uint32_t */
@@ -503,10 +755,19 @@ struct MpscRing {
         nr_size = this->size * scale;
         this->dequeue_elems_32(nr_size, nr_idx, entries, nr_num);
     }
+
   public:
     static inline Allocator allocator;
 
-    static MpscRing* of(uint32_t cnt) {
+    static void free(Ring* ring) {
+        allocator_traits::deallocate(
+            allocator,
+            reinterpret_cast<uint8_t*>(ring),
+            ring->memzone_.size_
+        );
+    }
+
+    static Ring* of(uint32_t cnt) {
         ssize_t sz;
         uint32_t esize = sizeof(T);
         auto count = sqk_align32pow2(cnt + 1);
@@ -519,42 +780,78 @@ struct MpscRing {
             throw std::system_error(EINVAL, std::system_category());
         }
 
-        sz = sizeof(MpscRing) + count * esize;
+        sz = sizeof(Ring) + count * esize;
         sz = SQK_ALIGN(sz, SQK_CACHE_LINESIZE);
 
         void* buf = allocator_traits::allocate(allocator, sz);
-        auto ring = static_cast<MpscRing*>(buf);
-        ring->memzone_.addr_ = static_cast<uint8_t*>(buf) + sizeof(MpscRing);
+        auto ring = static_cast<Ring*>(buf);
         ring->init(cnt);
+        ring->memzone_.addr_ = static_cast<uint8_t*>(buf) + sizeof(Ring);
+        ring->memzone_.size_ = sz;
 
         return ring;
     }
 
     template<typename T2>
     uint32_t enqueue(T2&& entry) {
-        uint32_t prod_head, prod_next;
-        uint32_t free_entries;
-        uint32_t n;
-        n = this->move_prod_head(1, prod_head, prod_next, free_entries);
-        if (!n) {
-            return 0;
+        if constexpr (prod_sync_type == RingSyncType::SQK_RING_SYNC_ST
+                      || prod_sync_type == RingSyncType::SQK_RING_SYNC_MT) {
+            uint32_t prod_head, prod_next;
+            uint32_t free_entries;
+            uint32_t n;
+            n = this->move_prod_head(1, prod_head, prod_next, free_entries);
+            if (!n) {
+                return 0;
+            }
+            this->enqueue_elements(prod_head, &entry, n);
+            this->update_tail(this->prod_, prod_head, prod_next, 0, 1);
+            return n;
+        } else if constexpr (prod_sync_type
+                             == RingSyncType::SQK_RING_SYNC_MT_HTS) {
+            uint32_t free, head;
+            uint32_t n;
+            n = move_prod_head(1, head, free);
+            if (n != 0) {
+                this->enqueue_elements(head, &entry, n);
+                this->update_tail(this->prod_, head, n, 1);
+            }
+            return n;
+        } else if constexpr (prod_sync_type
+                             == RingSyncType::SQK_RING_SYNC_MT_RTS) {
+            assert(0);
         }
-        this->enqueue_elements(prod_head, &entry, n);
-        this->update_tail(prod_head, prod_next, 0, n);
-        return n;
     }
 
     uint32_t dequeue(T& entry) {
-        uint32_t cons_head, cons_next;
-        uint32_t entries;
+        if constexpr (cons_sync_type == RingSyncType::SQK_RING_SYNC_ST
+                      || cons_sync_type == RingSyncType::SQK_RING_SYNC_MT) {
+            uint32_t cons_head, cons_next;
+            uint32_t entries;
 
-        uint32_t n = this->move_cons_head(1, cons_head, cons_next, entries);
-        if (!n) {
-            return 0;
+            uint32_t n = this->move_cons_head(1, cons_head, cons_next, entries);
+            if (!n) {
+                return 0;
+            }
+            this->dequeue_elements(cons_head, &entry, n);
+            this->update_tail(this->cons_, cons_head, cons_next, 1, 0);
+            return n;
+        } else if constexpr (cons_sync_type
+                             == RingSyncType::SQK_RING_SYNC_MT_HTS) {
+            uint32_t entries, head;
+            uint32_t n;
+
+            n = move_cons_head(1, head, entries);
+
+            if (n != 0) {
+                this->dequeue_elements(head, &entry, n);
+                this->update_tail(this->cons_, head, n, 0);
+            }
+            return n;
+
+        } else if constexpr (cons_sync_type
+                             == RingSyncType::SQK_RING_SYNC_MT_RTS) {
+            assert(0);
         }
-        this->dequeue_elements(cons_head, &entry, n);
-        this->update_tail(cons_head, cons_next, 1, 0);
-        return n;
     }
 };
 
--- src/tests/common/ring_test.cc
+++ src/tests/common/ring_test.cc
@@ -1,14 +1,63 @@
 #include "ring.hpp"
-int main (int argc, char *argv[]) {
+
+int main(int argc, char* argv[]) {
     S_LOGGER_SETUP;
-    auto ring = MpscRing<int>::of(10);
-    ring->enqueue(1);
-    int i = 0xdd;
-    ring->dequeue(i);
-    if (i == 1) {
-        return 0;
-    } else {
-        S_ERROR("i={}", i);
-        return 1;
+    {
+        auto ring = Ring<
+            int,
+            RingSyncType::SQK_RING_SYNC_MT,
+            RingSyncType::SQK_RING_SYNC_MT>::of(10);
+        ring->enqueue(1);
+        int i = 0xdd;
+        ring->dequeue(i);
+        std::remove_reference_t<decltype(*ring)>::free(ring);
+        if (i != 1) {
+            S_ERROR("i={}", i);
+            return 1;
+        }
     }
+    {
+        auto ring = Ring<
+            int,
+            RingSyncType::SQK_RING_SYNC_MT_HTS,
+            RingSyncType::SQK_RING_SYNC_MT_HTS>::of(10);
+        ring->enqueue(1);
+        int i = 0xdd;
+        ring->dequeue(i);
+        std::remove_reference_t<decltype(*ring)>::free(ring);
+        if (i != 1) {
+            S_ERROR("i={}", i);
+            return 1;
+        }
+    }
+    {
+        auto ring = Ring<
+            int,
+            RingSyncType::SQK_RING_SYNC_MT,
+            RingSyncType::SQK_RING_SYNC_MT_HTS>::of(10);
+        ring->enqueue(1);
+        int i = 0xdd;
+        ring->dequeue(i);
+        std::remove_reference_t<decltype(*ring)>::free(ring);
+        if (i != 1) {
+            S_ERROR("i={}", i);
+            return 1;
+        }
+    }
+    {
+        auto ring = Ring<
+            int,
+            RingSyncType::SQK_RING_SYNC_MT_HTS,
+            RingSyncType::SQK_RING_SYNC_MT>::of(10);
+        ring->enqueue(1);
+        int i = 0xdd;
+        ring->dequeue(i);
+        std::remove_reference_t<decltype(*ring)>::free(ring);
+        if (i != 1) {
+            S_ERROR("i={}", i);
+            return 1;
+        }
+    }
+
+    return 0;
 }
